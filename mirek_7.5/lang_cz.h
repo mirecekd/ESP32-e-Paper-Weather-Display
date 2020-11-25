b5b221f079357b2bf2018f3d16696aa0fbf8fbd5
dmirek <dvorak@poda.cz>
2020-11-25 14:30:30 +0100

    my changes

---
+++ mirek_7.5/lang_cz.h
@@ -0,0 +1,70 @@
+#define FONT(x) x##_te
+
+//Temperature - Humidity - Forecast
+const String TXT_FORECAST_VALUES  = "Predpoved na dalsich 72 hodin (3 dny)";
+const String TXT_CONDITIONS       = "Aktualni situace";
+const String TXT_DAYS             = "(Dny)";
+const String TXT_TEMPERATURES     = "Teplota";
+const String TXT_TEMPERATURE_C    = "Teplota (*C)";
+const String TXT_TEMPERATURE_F    = "Teplota (*F)";
+const String TXT_HUMIDITY_PERCENT = "Vlhkost (%)";
+
+// Pressure
+const String TXT_PRESSURE         = "Tlak";
+const String TXT_PRESSURE_HPA     = "Tlak (hPa)";
+const String TXT_PRESSURE_IN      = "Tlak (in)";
+const String TXT_PRESSURE_STEADY  = "Konstantni";
+const String TXT_PRESSURE_RISING  = "Stoupajici";
+const String TXT_PRESSURE_FALLING = "Klesajici";
+
+//RainFall / SnowFall
+const String TXT_RAINFALL_MM = "Srazky (mm)";
+const String TXT_RAINFALL_IN = "Srazky (in)";
+const String TXT_SNOWFALL_MM = "Snezeni (mm)";
+const String TXT_SNOWFALL_IN = "Snezeni (in)";
+const String TXT_PRECIPITATION_SOON = "Srazky";
+
+//Sun
+const String TXT_SUNRISE  = "slunce vychazi";
+const String TXT_SUNSET   = "slunce zapada";
+
+//Moon
+const String TXT_MOON_NEW             = "Mesic je v novu";
+const String TXT_MOON_WAXING_CRESCENT = "Mesic ma dorustajici srpek";
+const String TXT_MOON_FIRST_QUARTER   = "Mesic je v prvni ctvrti";
+const String TXT_MOON_WAXING_GIBBOUS  = "Mesic dorusta";
+const String TXT_MOON_FULL            = "Mesic je v uplnku";
+const String TXT_MOON_WANING_GIBBOUS  = "Mesic couva";
+const String TXT_MOON_THIRD_QUARTER   = "Mesic je v posledni ctvrti";
+const String TXT_MOON_WANING_CRESCENT = "Mesic zbyva maly srpek";
+
+//Power / WiFi
+const String TXT_POWER  = "Baterka";
+const String TXT_WIFI   = "WiFi";
+const char* TXT_UPDATED = "Posledni aktualizace";
+
+
+//Wind
+const String TXT_WIND_SPEED_DIRECTION = "Rychlost a smer vetru";
+const String TXT_N   = "S";
+const String TXT_NNE = "SSV";
+const String TXT_NE  = "SV";
+const String TXT_ENE = "VSV";
+const String TXT_E   = "V";
+const String TXT_ESE = "VJV";
+const String TXT_SE  = "JV";
+const String TXT_SSE = "JJV";
+const String TXT_S   = "J";
+const String TXT_SSW = "JJZ";
+const String TXT_SW  = "JZ";
+const String TXT_WSW = "ZJZ";
+const String TXT_W   = "Z";
+const String TXT_WNW = "ZSZ";
+const String TXT_NW  = "SZ";
+const String TXT_NNW = "SSZ";
+
+//Day of the week
+const char* weekday_D[] = { "Nedele", "Pondeli", "Utery", "Streda", "Ctvrtek", "Patek", "Sobota" };
+
+//Month
+const char* month_M[] = { "leden", "unor", "brezen", "duben", "kveten", "cerven", "cervenec", "srpen", "zari", "rijen", "listopad", "prosinec" };
